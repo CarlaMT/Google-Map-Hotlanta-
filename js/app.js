4ddf811b90dc90d8f102686c5cd73f68d4a3cb1a
Carla MT <girlcodeprojx@gmail.com>
2018-09-11 11:30:28 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,36 +1,34 @@
 var Location = function(data) {
     this.title = data.title;
     this.location = data.location;
-
 };
 
 var ViewModel = function() {
-    var self = this;
+    var me = this;
 
-    self.locationsList = ko.observableArray([]);
-
+    me.locationsList = ko.observableArray([]);
     locations.forEach(function(location) {
-        self.locationsList.push(new Location(location));
+        me.locationsList.push(new Location(location));
     });
     //Will show when Google Maps will be unavailable
-    self.mapError = ko.observable(false);
+    me.mapError = ko.observable(true);
 
-    self.filter = ko.observable('');
-    self.filteredLocations = ko.computed(function() {
-        var filterResult = self.filter().toLowerCase();
+    me.filter = ko.observable('');
+    me.filteredLocations = ko.computed(function() {
+        var filterResult = me.filter().toLowerCase();
 
         if (!filterResult) {
-            for (var i = 0; i < self.locationsList().length; i++) {
-                //When this runs first time there are no markers yet,therefore checking
-                // whether they defined, if yes set them visible so we are back to
-                //square one
-                if (self.locationsList()[i].marker) {
-                    self.locationsList()[i].marker.setVisible(true);
+            for (var i = 0; i < me.locationsList().length; i++) {
+                //After running code the first time there are no 
+                //visible markers after checking if markers were defined.
+                //After verification, resets to visible. 
+                if (me.locationsList()[i].marker) {
+                    me.locationsList()[i].marker.setVisible(true);
                 }
             } //end for loop
-            return self.locationsList();
+            return me.locationsList();
         } else {
-            return ko.utils.arrayFilter(self.locationsList(), function(loc) {
+            return ko.utils.arrayFilter(me.locationsList(), function(loc) {
                 // test to see if item matches filter and store results as a variable
                 var match = loc.title.toLowerCase().indexOf(filterResult) >= 0;
                 // set marker visibility based on match status
@@ -42,29 +40,32 @@ var ViewModel = function() {
                 return match;
             });
         }
-    }, self);
+    }, me);
 
-    self.clearFilter = function() {
-        self.filter('');
+    me.clearFilter = function() {
+        me.filter('');
 
-        for (var i = 0; i < self.locationsList().length; i++) {
+        for (var i = 0; i < me.locationsList().length; i++) {
             //get all the markers back
-            self.locationsList()[i].marker.setVisible(true);
+            me.locationsList()[i].marker.setVisible(true);
         }
     };
 
+    //* ***************** * **********************************
+    // Side Bar Array | Selection of location with InfoWindow
+    //* ***************** * **********************************
     self.currentLocation = ko.observableArray([this.locationsList()[0]]);
-    self.selectLocation = function(clickedLocation) {
-        self.currentLocation(clickedLocation);
-        self.animateUponClick = function(clickedLocation) {
-            populateInfoWindow(clickedLocation.marker, infoWindow);
-        }
+    this.selectLocation = function(clickedLocation) {
+        //sets the current location(clickedLocation);
+        animateUponClick(clickedLocation.marker);
+        //populating the info window by clicked marker from list
+        populateInfoWindow(clickedLocation.marker, infoWindow);
     };
     //Observable for Menu | Navigation Bar Toggle Button
     self.visibleMenu = ko.observable(false),
         //Showing/Hiding the menu
         self.clickMe = function() {
-            this.visibleMenu(!this.visibleMenu());
+            self.visibleMenu(!self.visibleMenu());
         };
 
 };
